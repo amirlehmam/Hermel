5db68f71eca6865d437942cf7b08ddfad0e0f3e3
Amir <aeonthings@gmail.com>
2023-10-15 17:52:12 +0200

    11

---
+++ Hermel/cold_emailing/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ColdEmailingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cold_emailing'
---
+++ app/django.py
@@ -0,0 +1,8 @@
+# Créer un nouveau projet
+django-admin startproject Hermel
+
+# Naviguer dans le répertoire du projet
+cd Hermel
+
+# Créer une nouvelle application
+python manage.py startapp cold_emailing
